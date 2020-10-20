7eec2e6a75593a1c433c47bef2ebde804e79669b
Pugsly99 <rylandadams@yahoo.com>
2020-10-20 13:29:29 -0700

    add c# clickable and def classes and id's
    Co-Authored By:Harrison Strand <harrisonstrand@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,132 @@
+$(document).ready(function() {
+  $(".clickable1").click(function() {
+    $("#card1").toggle();
+    $("#def1").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $(".clickable2").click(function() {
+    $("#card2").toggle();
+    $("#def2").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $(".clickable3").click(function() {
+    $("#card3").toggle();
+    $("#def3").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $(".clickable4").click(function() {
+    $("#card4").toggle();
+    $("#def4").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $(".clickable5").click(function() {
+    $("#card5").toggle();
+    $("#def5").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $(".clickable6").click(function() {
+    $("#card6").toggle();
+    $("#def6").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $(".clickable7").click(function() {
+    $("#card7").toggle();
+    $("#def7").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $(".clickable8").click(function() {
+    $("#card8").toggle();
+    $("#def8").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $(".clickable9").click(function() {
+    $("#card9").toggle();
+    $("#def9").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $(".clickable10").click(function() {
+    $("#card10").toggle();
+    $("#def10").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $(".clickable11").click(function() {
+    $("#card11").toggle();
+    $("#def11").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $(".clickable12").click(function() {
+    $("#card12").toggle();
+    $("#def12").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $(".clickable13").click(function() {
+    $("#card13").toggle();
+    $("#def13").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $(".clickable14").click(function() {
+    $("#card14").toggle();
+    $("#def14").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $(".clickable15").click(function() {
+    $("#card15").toggle();
+    $("#def15").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $(".clickable16").click(function() {
+    $("#card16").toggle();
+    $("#def16").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $(".clickable17").click(function() {
+    $("#card17").toggle();
+    $("#def17").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $(".clickable18").click(function() {
+    $("#card18").toggle();
+    $("#def18").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $(".clickable19").click(function() {
+    $("#card19").toggle();
+    $("#def19").toggle();
+  });
+});
